e445cda30da66adf1cd7eddee0941f938a1272df
diegosskiii <di.ormazabal@duocuc.cl>
2024-04-20 15:16:18 -0400

    modificacion urls user

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('product/api/', include('apps.product.routers'))
+    path('product/api/', include('apps.product.routers')),
+    path('user/api/', include('apps.user.urls')),
 
 ]
