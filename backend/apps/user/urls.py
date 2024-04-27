97a3bcef0f45fd5e5a6237bc12dcc65ac054cbc9
diegosskiii <di.ormazabal@duocuc.cl>
2024-04-27 12:17:16 -0400

    login

--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from apps.user.views import user_api_view, user_detail_view
+from apps.user.views import user_api_view, user_detail_view, Login
 
 urlpatterns = [
     path('users/', user_api_view, name='user_api'),
+    path('', Login.as_view(),name='Login')
     path('detail/<int:pk>/', user_detail_view, name='user_detail_api')
 ]
--- backend/apps/user/views.py
+++ backend/apps/user/views.py
@@ -1,9 +1,21 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from .serializer import UserSerializer, UserListSerializer
 from .models import User
+from rest_framework.authtoken.views import ObtainAuthToken 
+
+#LOGIN
+class Login(ObtainAuthToken):
+    def post(self,request,*args,**kwargs):
+        login_serializer = self.serializer_class(data = request.data, context = {'request':request})
+        if login_serializer.is_valid():
+            print("Pasó validación")
+            return Response({'mensaje':'Hola desde response'}, status= status.HTTP_200_OK)
+
+
+
+
 
 @api_view(['GET','POST'])
 def user_api_view(request):
